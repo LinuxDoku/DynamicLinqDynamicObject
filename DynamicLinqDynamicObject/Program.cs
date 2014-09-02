5923a659815dcf6327193794b6e39f925ecded29
Martin Lantzsch <martin@linux-doku.de>
2014-09-02 08:13:55 +0200

    initial commit

---
+++ DynamicLinqDynamicObject/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq.Dynamic;
+using System.Collections.Generic;
+using System.Security;
+using System.Linq;
+using System.Dynamic;
+using System.Linq.Expressions;
+
+namespace DynamicLinqDynamicObject
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            var list = new List<Data>();
+
+            var dataInstance = new Data();
+            list.Add(new Data()
+            {
+                Storage = new Data()
+                {
+                    Name = "Hello World"
+                },
+                Name = "Test"
+            });
+
+            list.Add(dataInstance);
+
+            Console.WriteLine(list.First().Storage.Name);
+
+            var result = list.Where("it.Storage != null").Select("it.Storage");
+            var res2 = result.Where("it.Name = @0", "Test");
+
+            Console.WriteLine(res2.ToString());
+        }
+    }
+}
---
+++ System.Linq.Dynamic/Data.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace System.Linq.Dynamic
+{
+    public class Data
+    {
+        public dynamic Storage;
+
+        public dynamic Name;
+    }
+}
